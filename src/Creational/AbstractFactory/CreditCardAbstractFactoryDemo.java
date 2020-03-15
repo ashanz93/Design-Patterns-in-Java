c56f9bdfee73a8d57158ea46ccec344d578b15c1
Ashwin <ashanz93@outlook.com>
2020-03-15 16:37:03 +0530

    Commit client class - AbstractFactory pattern

---
+++ src/Creational/AbstractFactory/CreditCardAbstractFactoryDemo.java
@@ -0,0 +1,16 @@
+package Creational.AbstractFactory;
+
+public class CreditCardAbstractFactoryDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CreditCardFactory abstractFactory = CreditCardFactory.getCreditCardFactory(775);
+		CreditCard card = abstractFactory.getCreditCard(CardType.PLATINUM);
+		System.out.println(card.getClass());
+		
+		abstractFactory = CreditCardFactory.getCreditCardFactory(600);
+		CreditCard card2 = abstractFactory.getCreditCard(CardType.PLATINUM);
+		System.out.println(card2.getClass());
+	}
+
+}
